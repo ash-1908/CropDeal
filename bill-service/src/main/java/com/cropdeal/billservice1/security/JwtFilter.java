fe736cdad0c56ea3f97d7b89478508077c1e42f0
Anmol Sharma <anmolsh.1908@gmail.com>
2022-09-13 16:05:45 +0530

    Added jwt filter to bill microservice

---
+++ bill-service/src/main/java/com/cropdeal/billservice1/security/JwtFilter.java
@@ -0,0 +1,62 @@
+package com.cropdeal.billservice1.security;
+
+import com.cropdeal.billservice1.entity.MyUserDetails;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+	private final JwtUtil jwtUtil;
+	
+	@Autowired
+	public JwtFilter(JwtUtil jwtUtil) {
+		this.jwtUtil = jwtUtil;
+	}
+	
+	private Boolean validateToken(String jwt) {
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.postForObject("http://localhost:8081/validate-token?token=" + jwt, null, Object.class);
+		return true;
+	}
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+		throws ServletException, IOException {
+//		get the jwt token from request and extract username from it
+		final String header = request.getHeader("Authorization");
+		String jwt_token = null;
+		if (header != null && header.startsWith("Bearer")) {
+			jwt_token = header.substring(7);
+		}
+		else throw new ServletException("Invalid JWT");
+//		check validity of the jwt token
+			if (validateToken(jwt_token)) {
+				final ObjectMapper mapper = new ObjectMapper();
+				Claims claims = jwtUtil.getAllClaimsFromToken(jwt_token);
+				MyUserDetails userDetails = mapper.convertValue(claims.get("accountInfo"), MyUserDetails.class);
+				System.out.println(userDetails);
+				UsernamePasswordAuthenticationToken authToken =
+					new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+				authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+				SecurityContextHolder.getContext().setAuthentication(authToken);
+			}
+		
+		filterChain.doFilter(request, response);
+	}
+}
--- bill-service/src/main/java/com/cropdeal/billservice1/security/SecurityConfiguration.java
+++ bill-service/src/main/java/com/cropdeal/billservice1/security/SecurityConfiguration.java
@@ -1,5 +1,29 @@
 package com.cropdeal.billservice1.security;
 
-/*public class SecurityConfiguration {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-}*/
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+	private final JwtFilter myJwtFilter;
+	
+	@Autowired
+	public SecurityConfiguration(JwtFilter myJwtFilter) {
+		this.myJwtFilter = myJwtFilter;
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeHttpRequests().anyRequest().permitAll().and().sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.addFilterBefore(myJwtFilter, UsernamePasswordAuthenticationFilter.class);
+		return http.build();
+	}
+}
