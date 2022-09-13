a2126212d3294db215d557f38071bf701b6bf59b
Anmol Sharma <anmolsh.1908@gmail.com>
2022-09-13 16:31:18 +0530

    Added jwt filter

---
+++ user-service/src/main/java/com/demo/cropdeal/user/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.demo.cropdeal.user.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+	private final JwtFilter myJwtFilter;
+	
+	@Autowired
+	public SecurityConfig(JwtFilter myJwtFilter) {
+		this.myJwtFilter = myJwtFilter;
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeHttpRequests().anyRequest().permitAll().and().sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.addFilterBefore(myJwtFilter, UsernamePasswordAuthenticationFilter.class);
+		return http.build();
+	}
+}
+
+
