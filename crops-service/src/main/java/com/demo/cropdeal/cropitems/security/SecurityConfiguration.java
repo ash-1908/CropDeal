9890d74483e4839e988523bcdc5cad537bc5f924
Anmol Sharma <anmolsh.1908@gmail.com>
2022-09-13 16:16:35 +0530

    Added jwt filter

---
+++ crops-service/src/main/java/com/demo/cropdeal/cropitems/security/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.demo.cropdeal.cropitems.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+	private final JwtFilter myJwtFilter;
+	
+	@Autowired
+	public SecurityConfiguration(JwtFilter myJwtFilter) {
+		this.myJwtFilter = myJwtFilter;
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeHttpRequests().anyRequest().permitAll().and().sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.addFilterBefore(myJwtFilter, UsernamePasswordAuthenticationFilter.class);
+		return http.build();
+	}
+}
--- crops-service/src/main/java/com/demo/cropdeal/cropitems/service/EmailSenderService.java
+++ crops-service/src/main/java/com/demo/cropdeal/cropitems/service/EmailSenderService.java
@@ -1,4 +1,4 @@
-package com.demo.cropdeal.cropitems;
+package com.demo.cropdeal.cropitems.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
