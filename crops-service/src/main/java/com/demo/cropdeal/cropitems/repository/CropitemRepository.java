d6adc7155676419fcea27d4199c99bf671083c64
Anmol Sharma <anmolsh.1908@gmail.com>
2022-09-16 09:29:09 +0530

    added mongo db support

--- crops-service/src/main/java/com/demo/cropdeal/cropitems/repository/CropitemRepository.java
+++ crops-service/src/main/java/com/demo/cropdeal/cropitems/repository/CropitemRepository.java
@@ -1,9 +1,11 @@
 package com.demo.cropdeal.cropitems.repository;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.demo.cropdeal.cropitems.model.Cropitem;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface CropitemRepository extends JpaRepository<Cropitem, Long> {
+public interface CropitemRepository extends MongoRepository<Cropitem, ObjectId> {
 
 }
--- crops-service/src/main/java/com/demo/cropdeal/cropitems/service/CropitemService.java
+++ crops-service/src/main/java/com/demo/cropdeal/cropitems/service/CropitemService.java
@@ -3,6 +3,7 @@ package com.demo.cropdeal.cropitems.service;
 import java.util.List;
 
 import com.demo.cropdeal.cropitems.model.Cropitem;
+import org.bson.types.ObjectId;
 
 
 //import com.cg.cropdeal.cropitem.model.TestcropItem;
@@ -11,8 +12,8 @@ public interface CropitemService {
 	//Defined the methods which is implemented in cropItemServiceImpl
 	Cropitem savecropItem(Cropitem cropitem);
 	List<Cropitem> getAllcropItems();
-	Cropitem getcropItemById(long cropid);
-	Cropitem updatecropItem(Cropitem cropitem,long cropid);
-	void deletecropItem(long cropid);
+	Cropitem getcropItemById(ObjectId cropid);
+	Cropitem updatecropItem(Cropitem cropitem,ObjectId cropid);
+	void deletecropItem(ObjectId cropid);
 	
 }
